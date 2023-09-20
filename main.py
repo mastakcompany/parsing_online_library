71e3021c9191cb1d8de819333f90c5a6687c65cf
Jora <joradesktop@joradesktop.com>
2023-09-20 13:36:36 +0700

    Added an option to specify the id of the books to download in the script arguments

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 from urllib.parse import urljoin, urlsplit
-
 import requests
+import argparse
 
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser(description='The program downloads a range of books, by their id')
+parser.add_argument('start_id', help='Initial book id', default=1, type=int, nargs='?')
+parser.add_argument('end_id', help='Final id of the book, not including it', default=10, type=int, nargs='?')
+args = parser.parse_args()
 
 
 def check_for_redirect(response):
@@ -52,16 +57,16 @@ def parse_book_page(html):
     comments = [comment.find('span', class_='black').text for comment in soup.find_all('div', class_='texts')]
     
     return {
-        'title': title,
-        'author': author,
-        'book_cover': book_cover,
+        'title'      : title,
+        'author'     : author,
+        'book_cover' : book_cover,
         'book_genres': book_genres,
         'description': description,
-        'comments': comments
+        'comments'   : comments
     }
-    
-    
-for i in range(5, 6):
+
+
+for i in range(args.start_id, args.end_id+1):
     try:
         response = requests.get(book_url.format(i), allow_redirects=False)
         response.raise_for_status()
@@ -70,15 +75,12 @@ for i in range(5, 6):
         print(f'Обнаружен редирект, книга c id: {i} не скачана!\n')
     else:
         soup = BeautifulSoup(response.text, 'lxml')
-        title = soup.find('h1').text.split('::')[0].strip()
-        url = download_book_url.format(i)
-        # download_txt(url, title)
-        
         book_cover = soup.find('div', class_='bookimage').find('img').get('src')
         book_cover_url = urljoin(base_url, book_cover)
-        # print(title, book_cover_url, sep='\n', end='\n\n')
-        # download_image(book_cover_url)
-        # print(f'Заголовок: {title}')
-        comments = [comment.find('span', class_='black').text for comment in soup.find_all('div', class_='texts')]
-        book_genres = [genres.text for genres in soup.find('span', class_='d_book').find_all('a')]
-        description = soup.find(id='content').find_all('table')[2].text
+        url = download_book_url.format(i)
+        book_data = parse_book_page(response)
+
+        title, author = book_data['title'], book_data['author']
+        download_txt(url, title)
+        download_image(book_cover_url)
+        print(f'Название: {title}\nАвтор: {author}', end='\n\n')
